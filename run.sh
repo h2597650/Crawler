1666fe7e7678759c932bd6a106b4d2f29e0b0a3e
h2597650 <shixue2007@qq.com>
2017-03-03 22:54:23 +0800

    modify run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 classpath="lib/htmlcleaner-2.8.jar:lib/json.jar:lib/jsoup-1.10.2.jar:lib/commons-lang-2.6.jar"
-javac -cp .:$classpath com/*.java
-java -cp .:$classpath com.LongCrawler
+find src -name *.java > src/sources.list
+
+
+javac -cp .:$classpath -s src -d bin @src/sources.list
+java -cp .:bin:$classpath com.LongCrawler
