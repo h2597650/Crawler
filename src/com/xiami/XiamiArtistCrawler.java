7d516dca3c5263d5384b0521ea35e762f6ad0fc3
Qiang Li <shixue2007@qq.com>
2017-03-10 15:49:34 +0800

    remove login

--- src/com/xiami/XiamiArtistCrawler.java
+++ src/com/xiami/XiamiArtistCrawler.java
@@ -491,9 +491,22 @@ public class XiamiArtistCrawler implements Runnable {
 				"O9fc383", "573", "2017", "135", "57908", "bhu21804", "hf0143fd", "sGE39222", "K1z4dbb3", 
 				"1198", "fHIe070b", "3110", "xIW446b0", "Ksd4688", "9K9c05a", "198ed78", "vuV4030c", "be6yda0f8", 
 				"dfTeba0e", "doO83b43", "iuzfce26", "cVv60212", "cUW5e566", "gaWaf903", "4q557b4", "c9b4b17c", 
-				"ixyea99c", "iwbf4790", "fGr9f340", "wz90661"};
+				"ixyea99c", "iwbf4790", "fGr9f340", "wz90661", "nb9Qfda9e", "nfZH1d73a", "1536810071", "7V3S1F1747c", 
+                "bifmb0a75", "CmK5492f9", "b08gREF3d702", "eFiI759ab", "dpew58809", "32414", "OEJ8d7cf", "bxeced2e2",
+                "iTnwc9824", "eHPra3049", "di8q3a59d", "32494", "rrc121d0a", "924056356", "4OZ51f7c", "rvD33b8b",
+                "dvX537b7e", "d4sE42b48", "yhDzyIad0f8", "eF5Q888dd", "iO0Pd6d61", "cV642fbeb", "mTlUKB7cdc9",
+                "UH9ddf74", "bkora23aa", "mhSfQb4d7f9", "Co0y45303", "cR752dc28", "yhDubac11f4", "3Ok7e759",
+                "bYPQ8ydaa27", "2100015558", "eGb14e269", "eHG469717", "x0Gsyyc5e8c", "bnvvbe4b2", "dI2242bd0",
+                "Z6Habad8", "wWmI9z99d19", "iQKpa1012", "b9EBfba81", "ciRl29bab", "bsYOebe4e", "eCOL60597",
+                "cT3d09c0", "ncAAe290a", "bdpvcad80", "wW71YZd5480", "ETI5c115", "eFAo7be88", "bvB3f29c5",
+                "PNK51ab9", "2100015572", "beZLdee43", "Wrt1617b", "2100015574", "bfNt14f43", "cxfSM5b7af0",
+                "9cy1cg19adc", "c1BD27b72", "Ndk63057", "blmqe0a79", "nl9yjA5ca43", "yhDyBkb0da9", "3AJ7301e",
+                "bF2N1b707", "roml20c2c", "3QIx3be6d", "bn8Zce270", "bFM1f5949", "90721953", "bu10ff670", "Ju77dff9",
+                "bbdbbd00a", "xIbotsc5908", "TFnced17", "eHkz6f6d0", "Wt8d5841", "zEYJ2dc0b", "bnF0cd8fa", "gN8pAD2e402",
+                "eDQn7bcab", "b99Wea3ad", "hTsZJA58935", "pacIsd535b", "fEvd6811", "iTqG125fe", "yhRutl12cdf", "b1tDfd939",
+                "cvDR2ac2d", "yhs81Gcba0a", "XUId575a", "8x0920c3", "bf1Da425f", "Z9Cc80d0", "bc6db6b12"};
 		XiamiArtistCrawler crawler = new XiamiArtistCrawler("http://www.xiami.com/artist/", artistIDs, 1000L, "xiami/");
-		crawler.login("15010700399","2517399LK");
+//		crawler.login("15010700399","2517399LK");
 //		crawler.setProxy("proxy.txt");
 		crawler.setProvider("provider.txt");
 		ArrayList<Thread> threads = new ArrayList<Thread>();
