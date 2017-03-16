6ce6a072471dd342c468765c6e4617d9fb1dde2c
h2597650 <shixue2007@qq.com>
2017-03-16 21:47:48 +0800

    m'

---
+++ run_xiami_mysql.sh
@@ -0,0 +1,8 @@
+classpath=@lib/classpath.txt
+find src -name *.java > src/sources.list
+
+mkdir -p bin
+javac -cp .:$classpath -s src -d bin @src/sources.list
+nohup java -cp .:bin:$classpath com.xiami.XiamiMysql > nohup.xiamimysql&
+sleep 3
+tail -f nohup.xiamimysql
