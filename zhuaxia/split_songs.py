cec12769c6c5d7cb372ae9c9f722a9b464840df9
Your Name <you@example.com>
2017-04-11 21:53:03 +0800

    m

--- zhuaxia/split_songs.py
+++ zhuaxia/split_songs.py
@@ -8,5 +8,5 @@ for i in range(int(math.ceil(len(vec)/float(s_size)))):
     file_name = 'split/%d-%d.txt' % (i*s_size, (i+1)*s_size)
     with open(file_name, 'w') as f:
         for idx in range(i*s_size, min(len(vec),(i+1)*s_size)):
-            f.write(vec[idx])
+            f.write('http://www.xiami.com/song/' + vec[idx])
 
